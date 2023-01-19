a8df17d4fac69525928e9bd26e6b2486fbab3c7e
Olivier <Olivier-maria@hotmail.com>
2023-01-19 16:56:33 +0100

    modification

---
+++ les_bases/k_return_11.rb
@@ -0,0 +1,14 @@
+#Les return
+
+def cube(num)
+ return num * num * num, 70
+ 5
+end
+
+puts cube(2)[1]
+
+#Le mot clef, Return permez d'informée ruby que nous en avons fini avec cette méthode. tout ce qui sera insérée entre return et end ne sera pas pris en compte lors de la lecture du programme.
+
+#[0] étant l'index, je lui demande la [1] de me sortir la velur ciblé. dans l'exemple ci-dessus 70. 
+
+#L'utilisation de return permet d'améliorer ses méthodes.
---
+++ les_bases/l_if_12.rb
@@ -0,0 +1,5 @@
+#If permet à notre programme de prendre des décissions.
+
+masculin = true
+
+if condition
